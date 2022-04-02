f275f4b756c4d0030cb96f0bd878b65622836706
fabioconejo <fabio.conejo@hotmail.com>
2022-04-02 19:42:14 -0300

    Base da identidade visual safra

---
+++ src/app/app.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor() { }
+  
+  fetchOpenAPI() {
+    
+  }
+
+  getPublicBankComparasion() : any {
+    return '';
+  }
+
+  getClientBankComparasion() : any{
+    return '';
+  }
+}
